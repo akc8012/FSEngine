048f2627f60dfada13f66b2c80e0ba2a2c4f807f
Andrew Colannino <akcolannino@email.neit.edu>
2018-06-02 23:37:10 -0400

    cleaned error handling

--- SDLGame/Engine.cpp
+++ SDLGame/Engine.cpp
@@ -12,7 +12,6 @@ bool Engine::init()
 	catch (string msg)
 	{
 		cout << msg << endl;
-		return false;;
 	}
 
 	SDL_UpdateWindowSurface(window);
@@ -25,18 +24,12 @@ SDL_Window* Engine::createWindow()
 {
 	SDL_Surface* surface = NULL;
 
-	if (SDL_Init(SDL_INIT_VIDEO) == -1)
-	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-		throw SDL_GetError();
-	}
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		throw (string)"SDL could not initialize! SDL_Error: " + SDL_GetError();
 
 	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (window == NULL)
-	{
-		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		throw SDL_GetError();
-	}
+		throw (string)"Window could not be created! SDL_Error: " + SDL_GetError();
 
 	surface = SDL_GetWindowSurface(window);
 	SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0x7F, 0xFF));
