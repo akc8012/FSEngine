93db5a5fadb6735f9ac0cc18e5b7a497acd6d37d
Andrew Colannino <akcolannino@email.neit.edu>
2018-08-07 19:11:17 -0400

    frag shader refactoring

--- FSEngine/Resource/Shader/FragmentPhongShader.glsl
+++ FSEngine/Resource/Shader/FragmentPhongShader.glsl
@@ -6,49 +6,16 @@ in vec3 FragmentPosition;
 in float RenderPerspective;
 
 uniform sampler2D diffuseTexture;
+uniform vec3 flatColor;
 uniform vec3 viewPosition;
 
 out vec4 FragmentColor;
 
-float ambientStrength = 0.5;
-float specularStrength = 1;
-float shininess = 32;
 
-vec3 lightColor = vec3(1, 1, 1);
-vec3 lightPosition = vec3(5, 5, 5);
-
-
-vec3 CalcAmbient()
-{
-	return lightColor * ambientStrength;
-}
-
-vec3 CalcDiffuse(vec3 normal, vec3 lightDirection)
-{
-	float diffuse = max(dot(normal, lightDirection), 0.0);
-	return diffuse * lightColor;
-}
-
-vec3 CalcSpecular(vec3 normal, vec3 lightDirection)
-{
-	vec3 viewDirection = normalize(-viewPosition - FragmentPosition);
-	vec3 reflectDirection = reflect(-lightDirection, normal);
-	float specular = pow(max(dot(viewDirection, reflectDirection), 0.0), shininess);
-
-	return specularStrength * specular * lightColor;
-}
-
-vec3 CalcLighting()
-{
-	vec3 normal = normalize(Normal);
-	vec3 lightDirection = normalize(lightPosition - FragmentPosition);
-
-	vec3 ambient = CalcAmbient();
-	vec3 diffuse = CalcDiffuse(normal, lightDirection);
-	vec3 specular = CalcSpecular(normal, lightDirection);
-
-	return ambient + diffuse + specular;
-}
+vec3 CalcLighting();
+vec3 CalcAmbient(vec3 lightColor);
+vec3 CalcDiffuse(vec3 normal, vec3 lightDirection, vec3 lightColor);
+vec3 CalcSpecular(vec3 normal, vec3 lightDirection, vec3 lightColor);
 
 void main()
 {
@@ -60,4 +27,43 @@ void main()
 
 	vec3 color = vec3(texture2D(diffuseTexture, TexureCoord));
 	FragmentColor = vec4(CalcLighting() * color, 1.0);
+}
+
+vec3 CalcLighting()
+{
+	vec3 normal = normalize(Normal);
+	vec3 lightColor = vec3(1, 1, 1);
+
+	vec3 lightPosition = vec3(5, 5, 5);
+	vec3 lightDirection = normalize(lightPosition - FragmentPosition);
+
+	vec3 ambient = CalcAmbient(lightColor);
+	vec3 diffuse = CalcDiffuse(normal, lightDirection, lightColor);
+	vec3 specular = CalcSpecular(normal, lightDirection, lightColor);
+
+	return ambient + diffuse + specular;
+}
+
+vec3 CalcAmbient(vec3 lightColor)
+{
+	float ambientStrength = 0.5;
+	return lightColor * ambientStrength;
+}
+
+vec3 CalcDiffuse(vec3 normal, vec3 lightDirection, vec3 lightColor)
+{
+	float diffuse = max(dot(normal, lightDirection), 0.0);
+	return diffuse * lightColor;
+}
+
+vec3 CalcSpecular(vec3 normal, vec3 lightDirection, vec3 lightColor)
+{
+	vec3 viewDirection = normalize(-viewPosition - FragmentPosition);
+	vec3 reflectDirection = reflect(-lightDirection, normal);
+
+	float shininess = 32;
+	float specular = pow(max(dot(viewDirection, reflectDirection), 0.0), shininess);
+
+	float specularStrength = 1;
+	return specularStrength * specular * lightColor;
 }
