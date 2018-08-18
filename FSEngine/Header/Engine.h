481992fe1594864beec62da0c03135625fd37cc2
Andrew Colannino <akcolannino@email.neit.edu>
2018-08-18 17:53:54 -0400

    Revert "Systems separate struct"
    This reverts commit 269a462365b05eada2463472838d887dfbcc460d.

--- FSEngine/Header/Engine.h
+++ FSEngine/Header/Engine.h
@@ -1,13 +1,49 @@
 #pragma once
-#include "Systems.h"
+#include "FileSystem.h"
+#include "Window.h"
+#include "ShaderProgram.h"
+#include "Renderer.h"
+#include "Timer.h"
+#include "GameObject.h"
+#include "SceneManager.h"
 #include "CubePrimitive.h"
 #include "RenderText.h"
 #include "Model.h"
 #include "PlayerShip.h"
 
-#include <string>
-using std::string;
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <GL\glew.h>
+#include <SDL_opengl.h>
 
+#include <string>
+
+#pragma region Systems
+struct Systems
+{
+private:
+	void InitSDL();
+	void InitOpenGl();
+	void InitGlew();
+
+public:
+	FileSystem* fileSystem = nullptr;
+	Window* window = nullptr;
+	ShaderProgram* shaderProgram = nullptr;
+	Input* input = nullptr;
+	Renderer* renderer = nullptr;
+	SceneManager* sceneManager = nullptr;
+
+	Systems();
+	~Systems();
+
+	void ToggleSwapInterval();
+	void SetSwapInterval(int interval);
+};
+#pragma endregion
+
+#pragma region Engine
 class Engine
 {
 private:
@@ -36,3 +72,4 @@ public:
 
 	SDL_Window* GetWindow() const;
 };
+#pragma endregion
