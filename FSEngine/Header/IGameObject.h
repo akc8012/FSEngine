c24adb6be8e6a496ae96372975d26339217eb63c
Andrew Colannino <akcolannino@email.neit.edu>
2018-10-20 14:12:18 -0400

    GetJson and SetJson for gameobject now defined in interface IGameObject

--- FSEngine/Header/IGameObject.h
+++ FSEngine/Header/IGameObject.h
@@ -33,6 +33,9 @@ public:
 
 	virtual ComponentContainer* GetComponentContainer() const = 0;
 
+	virtual json GetJson() const = 0;
+	virtual void SetFromJson(const json& j) = 0;
+
 	template <typename T>
 	T* AddComponent(shared_ptr<T> component, const string& name = "");
 	template <typename T>
