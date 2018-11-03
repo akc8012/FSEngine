75ac1af4bd80ec5da9a7431fa69c8aaac796c6e4
Andrew Colannino <akcolannino@email.neit.edu>
2018-11-03 19:46:14 -0400

    Janky solution for RenderText runtime add bug: get surface from settings

--- FSEngine/Source/RenderText.cpp
+++ FSEngine/Source/RenderText.cpp
@@ -36,6 +36,11 @@ void RenderText::ReceiveEvent(const string& key, const json& event)
 	{
 		if (key == "SurfaceSizeChanged")
 			surfaceSize = vec2 { event[0], event[1] };
+		else if (surfaceSize == vec2(0, 0)) // TODO: Make this less terrible
+		{
+			json jsonSurfaceSize = systems->fileSystem->GetSettingsValue("SurfaceSize");
+			surfaceSize = vec2(jsonSurfaceSize[0], jsonSurfaceSize[1]);
+		}
 
 		GetComponent<Transform2D>("Transform")->SetTransform(surfaceSize, GetComponent<FontTexture>("Shading")->GetSurfaceSize());
 	}
