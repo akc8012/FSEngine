af8c83bfdadae1c1119c0d4eabccdc5da50b2b97
Andrew Colannino <akcolannino@email.neit.edu>
2018-11-10 15:31:48 -0500

    Engine SDL event handling refactored to go through EventSystem

--- FSEngine/Source/Engine.cpp
+++ FSEngine/Source/Engine.cpp
@@ -204,18 +204,11 @@ void Engine::HandleWindowEvent(const SDL_WindowEvent& windowEvent)
 			SetOpenGlParameters();
 		}
 
-		if (systems->fileSystem->GetSettingsValue<bool>("LoadShadersOnFocus"))
-			renderer->ReCompileShaders();
-
-		if (systems->fileSystem->GetSettingsValue<bool>("LoadSceneOnFocus"))
-			sceneManager->GetCurrentScene()->LoadScene();
-
+		systems->eventSystem->SendEvent("WindowFocusGained");
 		break;
 
 	case SDL_WINDOWEVENT_FOCUS_LOST:
-		if (systems->fileSystem->GetSettingsValue<bool>("LoadSceneOnFocus"))
-			sceneManager->GetCurrentScene()->SaveScene();
-
+		systems->eventSystem->SendEvent("WindowFocusLost");
 		break;
 	}
 }
