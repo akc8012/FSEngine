9a70e46f8c2f1ce6c2f51cf9d3ebc68daeb9daac
Andrew Colannino <akcolannino@email.neit.edu>
2018-11-11 16:52:16 -0500

    work-in-progress ClickLabelManager spawn ClickLabels

--- FSEngine/Header/ClickLabelManager.h
+++ FSEngine/Header/ClickLabelManager.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "GameObject.h"
+#include "ClickLabel.h"
 
 class ClickLabelManager : public GameObject
 {
 public:
 	void Start() override;
+	void SceneLoaded() override;
 
 	string GetGameObjectType() const override;
 };
--- FSEngine/Source/ClickLabelManager.cpp
+++ FSEngine/Source/ClickLabelManager.cpp
@@ -5,6 +5,24 @@ void ClickLabelManager::Start()
 	GetParameterCollection()->SetParameter(DoDraw, false);
 }
 
+void ClickLabelManager::SceneLoaded()
+{
+	for (const auto gameObject : gameObjectContainer->GetGameObjects())
+	{
+		if (gameObject->TryGetComponent<Transform>() == nullptr)
+			continue;
+
+		auto clickLabel = gameObjectContainer->AddGameObject(gameObject->GetName() + "Label", make_unique<ClickLabel>());
+		clickLabel->SetSerializable(false);
+		clickLabel->AddComponent(make_shared<Color>(vec3(0.2f, 0.2f, 0.5f)), "Shading");
+		clickLabel->GetComponent<Transform>()->SetPosition(gameObject->GetComponent<Transform>()->GetPosition() + FSMath::Up);
+
+		clickLabel->SceneLoaded();
+	}
+
+	printFS("click label manager loaded");
+}
+
 string ClickLabelManager::GetGameObjectType() const
 {
 	return "ClickLabelManager";
