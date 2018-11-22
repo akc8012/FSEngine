d0c5cec97922b36761d4e27e268281d27786937c
Andrew Colannino <akcolannino@email.neit.edu>
2018-11-22 16:39:31 -0500

    SceneEditor TranslateActiveGameObject (x and z only so far)

--- FSEngine/Header/SceneEditor.h
+++ FSEngine/Header/SceneEditor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IUpdatable.h"
 #include "Scene.h"
+#include "Camera.h"
 #include "ClickLabelManager.h"
 
 class SceneEditor : public IUpdatable
@@ -8,9 +9,11 @@ class SceneEditor : public IUpdatable
 private:
 	Scene* scene = nullptr;
 	Systems* systems = nullptr;
+	Camera* camera = nullptr;
 
-	IGameObject* activeGameObject = nullptr;
 	unique_ptr<ClickLabelManager> clickLabelManager;
+
+	void TranslateActiveGameObject(IGameObject* activeGameObject);
 
 public:
 	SceneEditor(Scene* scene, Systems* systems);
--- FSEngine/Source/SceneEditor.cpp
+++ FSEngine/Source/SceneEditor.cpp
@@ -10,10 +10,28 @@ SceneEditor::SceneEditor(Scene* scene, Systems* systems)
 void SceneEditor::InitializeEditor()
 {
 	clickLabelManager->CreateClickLabels();
+	camera = scene->GetGameObjectContainer()->GetGameObjectAs<Camera>("Camera");
 }
 
 void SceneEditor::Update()
 {
 	clickLabelManager->Update();
-	activeGameObject = clickLabelManager->GetActiveGameObject();
+
+	if (systems->input->IsButtonHeld(SDL_SCANCODE_LSHIFT))
+		TranslateActiveGameObject(clickLabelManager->GetActiveGameObject());
+}
+
+void SceneEditor::TranslateActiveGameObject(IGameObject* activeGameObject)
+{
+	if (activeGameObject == nullptr)
+		return;
+
+	auto transform = activeGameObject->GetComponent<Transform>();
+	auto floorPlane = plane(vec3(0, transform->GetPosition().y, 0), FSMath::Up);
+	auto cursorRay = camera->GetCursorRay();
+
+	auto distance = FSMath::RayIntersectPlaneDistance(floorPlane, cursorRay);
+	auto position = vec3(cursorRay.origin + (cursorRay.direction * distance));
+
+	transform->SetPosition(position);
 }
